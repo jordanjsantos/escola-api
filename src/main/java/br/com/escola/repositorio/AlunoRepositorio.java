9ab3f01ca99cb28900f399cdaab8710c682d5d52
Jordan Santos <jrdnsantos@gmail.com>
2024-06-08 16:34:47 -0300

    Alterando tipo do id para UUID

--- src/main/java/br/com/escola/repositorio/AlunoRepositorio.java
+++ src/main/java/br/com/escola/repositorio/AlunoRepositorio.java
@@ -1,8 +1,10 @@
 package br.com.escola.repositorio;
 
 import br.com.escola.entidade.Aluno;
-import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlunoRepositorio extends JpaRepository<Aluno,Long> {
+import java.util.UUID;
+
+public interface AlunoRepositorio extends JpaRepository<Aluno, UUID> {
+
 }
