1be7af6bff2a7e3d16bdf113598c6c9ceaab3f1e
Jordan Santos <jrdnsantos@gmail.com>
2024-06-08 16:33:55 -0300

    Adicionando serialVersionUID para controle de versionamento de objetos

--- src/main/java/br/com/escola/entidade/Aluno.java
+++ src/main/java/br/com/escola/entidade/Aluno.java
@@ -20,6 +20,8 @@ import java.util.UUID;
 @NoArgsConstructor
 @Data
 public class Aluno implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
