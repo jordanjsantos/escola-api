44b094ada6243b4d689324c4d10cc745b58f533d
Carlos Roberto <crrsj1@gmail.com>
2024-06-08 11:31:11 -0300

    criando a interface AlunoRepositorio para interação com o banco de dados

---
+++ src/main/java/br/com/escola/repositorio/AlunoRepositorio.java
@@ -0,0 +1,8 @@
+package br.com.escola.repositorio;
+
+import br.com.escola.entidade.Aluno;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepositorio extends JpaRepository<Aluno,Long> {
+}
