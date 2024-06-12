b7685bd504d1aa145ebc781f1db3d9c275240852
Carlos Roberto <crrsj1@gmail.com>
2024-06-12 10:06:18 -0300

    inserindo as anotações do swagger

--- src/main/java/br/com/escola/EscolaApiApplication.java
+++ src/main/java/br/com/escola/EscolaApiApplication.java
@@ -1,9 +1,20 @@
 package br.com.escola;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API-Escola",
+				version = "1.0",
+				description = "Documentando uma API para aprendizado"
+
+		)
+)
 public class EscolaApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/escola/controle/AlunoControle.java
+++ src/main/java/br/com/escola/controle/AlunoControle.java
@@ -3,6 +3,10 @@ package br.com.escola.controle;
 
 import br.com.escola.dto.AlunoDto;
 import br.com.escola.servico.AlunoServico;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +25,10 @@ public class AlunoControle {
     private final AlunoServico servico;
 
     @PostMapping
+    @Operation(summary = "Rota responsável pelo cadastro de alunos")
+    @ApiResponse(responseCode = "201",description = "aluno cadastrado com sucesso",content = {
+            @Content(mediaType = "application.json",schema = @Schema(implementation = ResponseEntity.class))
+    })
     public ResponseEntity<AlunoDto> cadastrarAluno(@RequestBody @Valid AlunoDto aluno) {
         var cadastroMatricula = servico.cadastrarAluno(aluno);
         var uri = ServletUriComponentsBuilder.fromCurrentRequest().path("aluno/{id}")
@@ -29,23 +37,39 @@ public class AlunoControle {
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Rota responsável pela busca de aluno pelo id")
+    @ApiResponse(responseCode = "200",description = " sucesso",content = {
+            @Content(mediaType = "application.json",schema = @Schema(implementation = ResponseEntity.class))
+    })
     public ResponseEntity<AlunoDto> buscarPorId(@PathVariable Long id) {
         var busca = servico.buscarPorId(id);
         return ResponseEntity.ok(new AlunoDto(busca));
     }
     @GetMapping
+    @Operation(summary = "Rota responsável pela busca de todos os alunos")
+    @ApiResponse(responseCode = "200",description = " sucesso",content = {
+            @Content(mediaType = "application.json",schema = @Schema(implementation = ResponseEntity.class))
+    })
     public ResponseEntity<List<AlunoDto>> buscarTodos() {
         var buscar = servico.buscarTodos().stream().map(AlunoDto::new).toList();
         return ResponseEntity.ok(buscar);
 
     }
     @PutMapping
+    @Operation(summary = "Rota responsável por atualizar o aluno pelo id")
+    @ApiResponse(responseCode = "200",description = " sucesso",content = {
+            @Content(mediaType = "application.json",schema = @Schema(implementation = ResponseEntity.class))
+    })
     public ResponseEntity<AlunoDto>atualizarAluno(@RequestBody @Valid AlunoDto aluno,@PathVariable Long id){
         var atualize = servico.atualizarAluno(aluno, id);
         return ResponseEntity.ok().body(new AlunoDto(atualize));
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Rota responsável por deletar o aluno pelo id")
+    @ApiResponse(responseCode = "200",description = " sucesso",content = {
+            @Content(mediaType = "application.json",schema = @Schema(implementation = ResponseEntity.class))
+    })
     public ResponseEntity<Void>excluir(@PathVariable Long id){
         servico.excluir(id);
         return ResponseEntity.noContent().build();
